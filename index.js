5d31354521065cef854d180cf3d4608dbd368dfc
julian1026 <juancaor1997@gmail.com>
2022-02-09 19:57:48 -0500

    rama master

---
+++ index.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+//process argument
+let [, , option, secret, nameortoken] = process.argv;
+
+if (!option || !secret || !nameortoken) {
+  console.error("missing arguments");
+}
+
+function signToken(payload, secret) {
+  return jwt.sign(payload, secret);
+}
+
+function verifyToken(token, secret) {
+  return jwt.verify(token, secret);
+}
+
+if (option == "sign") {
+  console.log(signToken({ sub: nameortoken }, secret));
+} else if (option == "verify") {
+  console.log(verifyToken(nameortoken, secret));
+} else {
+  console.log("option needs to be 'sign' or 'verify' ");
+}
